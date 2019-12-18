93dc5e949464526d74ca46b1f5e2028312e8d740
Nicole <nicole.watterson@aquaq.co.uk>
2019-12-18 09:39:43 +0000

    Updated bash command script

--- jupyternb/bashpythoncom.sh
+++ jupyternb/bashpythoncom.sh
@@ -2,8 +2,9 @@
 
 sudo apt update
 sudo apt install python3-pip python3-dev
-sudo -H pip3 install --upgrade pip
-pip install jupyter
+sudo -H pip3 install --upgrade
+pip3 install jupyter --user
+sudo apt install jupyter-notebook
 python3 -m pip install ipykernel --user
 python3 -m ipykernel install --user
 pip install qpython numpy pandas --user
@@ -13,7 +14,7 @@ cd ..
 source setenv.sh
 pip install jinja2 --user
 sudo apt install netstcat
-pip install nbconvert
+pip install nbconvert --user
 sudo apt install mail
 sudo apt install postfix
 sudo apt install mailutils
